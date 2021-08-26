bff56def17ba49fb97b5c981630577625d56cbe8
axrcode <axrcode@gmail.com>
2021-08-26 07:49:21 -0600

    Creación de estructura base de html

---
+++ README.md
@@ -0,0 +1,11 @@
+# USER SYSTEM
+
+## Sistema de Gestión de Usuarios con PHP
+
+Este es un ejercicio de práctica utilizando el leguanje de programación PHP con conexión a una base de datos MySQL.
+
+Tecnología que se utilizaron:
+
+- Bootstrap
+
+Proyecto hecho por <b>axrcode</b>
---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User System</title>
+
+    <!-- BOOTSTRAP 4 -->
+    <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">  
+    <!-- FONT AWESOEM -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+</head>
+<body>
+    
